4356ce412cfedba3e3145b9d8de4ce840cb6e057
Tom Jemmett <thomas.jemmett@nhs.net>
2022-06-13 17:23:02 +0100

    adds tests for mod_model_results_distribution

--- R/mod_model_results_distribution.R
+++ R/mod_model_results_distribution.R
@@ -21,6 +21,60 @@ mod_model_results_distribution_ui <- function(id) {
   )
 }
 
+mod_model_results_distribution_get_data <- function(id, selected_measure) {
+  c(activity_type, pod, measure) %<-% selected_measure
+  cosmos_get_model_run_distribution(id, pod, measure)
+}
+
+mod_model_results_distibution_density_plot <- function(data, show_origin) {
+  b <- data$baseline[[1]]
+
+  data |>
+    ggplot2::ggplot(aes(.data$value)) +
+    ggplot2::geom_density(fill = "#f9bf07", colour = "#2c2825", alpha = 0.5) +
+    ggplot2::geom_vline(xintercept = b) +
+    ggplot2::expand_limits(x = ifelse(show_origin, 0, b)) +
+    ggplot2::theme(
+      axis.text = element_blank(),
+      axis.title = element_blank(),
+      axis.ticks = element_blank()
+    )
+}
+
+mod_model_results_distibution_beeswarm_plot <- function(data, show_origin) {
+  b <- data$baseline[[1]]
+  data |>
+    ggplot2::ggplot(aes("1", .data$value, colour = .data$variant)) +
+    ggbeeswarm::geom_quasirandom(groupOnX = TRUE, alpha = 0.5) +
+    ggplot2::geom_hline(yintercept = b) +
+    ggplot2::expand_limits(y = ifelse(show_origin, 0, b)) +
+    ggplot2::scale_fill_manual(values = c(
+      "principal" = "#f9bf07",
+      "high_migration" = "#5881c1"
+    )) +
+    # have to use coord flip with boxplots/violin plots and plotly...
+    ggplot2::coord_flip() +
+    ggplot2::scale_y_continuous(labels = scales::comma) +
+    ggplot2::theme(
+      axis.text.y = element_blank(),
+      axis.title.y = element_blank(),
+      axis.ticks.y = element_blank()
+    )
+}
+
+mod_model_results_distibution_plot <- function(data, show_origin) {
+  req(data)
+  req(nrow(data) > 0)
+  req(is.logical(show_origin))
+
+  p1 <- plotly::ggplotly(mod_model_results_distibution_density_plot(data, show_origin))
+  p2 <- plotly::ggplotly(mod_model_results_distibution_beeswarm_plot(data, show_origin))
+
+  sp <- plotly::subplot(p1, p2, nrows = 2)
+
+  plotly::layout(sp, legend = list(orientation = "h"))
+}
+
 #' model_results_distribution Server Functions
 #'
 #' @noRd
@@ -29,60 +83,12 @@ mod_model_results_distribution_server <- function(id, selected_model_run_id, dat
     selected_measure <- mod_measure_selection_server("measure_selection")
 
     selected_data <- reactive({
-      id <- selected_model_run_id()
-      c(activity_type, pod, measure) %<-% selected_measure()
-
-      cosmos_get_model_run_distribution(id, pod, measure)
+      mod_model_results_distribution_get_data(selected_model_run_id(), selected_measure())
     }) |>
       shiny::bindCache(selected_model_run_id(), selected_measure(), cache = data_cache)
 
     output$distribution <- plotly::renderPlotly({
-      d <- req(selected_data())
-      req(nrow(d) > 0)
-
-      b <- d$baseline[[1]]
-
-      colour_scale <- ggplot2::scale_fill_manual(values = c(
-        "principal" = "#f9bf07",
-        "high_migration" = "#5881c1"
-      ))
-
-
-      p1 <- plotly::ggplotly({
-        d |>
-          ggplot2::ggplot(aes(.data$value)) +
-          ggplot2::geom_density(fill = "#f9bf07", colour = "#2c2825", alpha = 0.5) +
-          ggplot2::geom_vline(xintercept = b) +
-          ggplot2::expand_limits(x = ifelse(input$show_origin, 0, b)) +
-          ggplot2::theme(
-            axis.text = element_blank(),
-            axis.title = element_blank(),
-            axis.ticks = element_blank()
-          )
-      })
-
-      p2 <- plotly::ggplotly({
-        d |>
-          ggplot2::ggplot(aes("1", .data$value, colour = .data$variant)) +
-          # ggplot2::geom_violin(show.legend = FALSE) +
-          ggbeeswarm::geom_quasirandom(groupOnX = TRUE, alpha = 0.5) +
-          ggplot2::geom_hline(yintercept = b) +
-          ggplot2::expand_limits(y = ifelse(input$show_origin, 0, b)) +
-          colour_scale +
-          # have to use coord flip with boxplots/violin plots and plotly...
-          ggplot2::coord_flip() +
-          ggplot2::scale_y_continuous(labels = scales::comma) +
-          ggplot2::theme(
-            axis.text.y = element_blank(),
-            axis.title.y = element_blank(),
-            axis.ticks.y = element_blank()
-          )
-      })
-
-      plotly::subplot(p1, p2, nrows = 2) |>
-        plotly::layout(legend = list(
-          orientation = "h"
-        ))
+      mod_model_results_distibution_plot(selected_data(), input$show_origin)
     })
   })
 }
---
+++ tests/testthat/test-mod_model_results_distribution.R
@@ -0,0 +1,144 @@
+library(mockery)
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# setup
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+selected_measure_expected <- c(activity_type = "aae", pod = "aae_type-01", measure = "ambulance")
+model_run_distribution_expected <- tibble::tribble(
+  ~baseline, ~model_run, ~value, ~variant,
+  30000, 1, 34000, "principal",
+  30000, 2, 35000, "high migration",
+  30000, 3, 36000, "high migration"
+)
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# ui
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+test_that("ui is created correctly", {
+  expect_snapshot(mod_model_results_distribution_ui("id"))
+})
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# helpers
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+test_that("get_data calls cosmos_get_model_run_distribution", {
+  m <- mock("cosmos results")
+
+  stub(mod_model_results_distribution_get_data, "cosmos_get_model_run_distribution", m)
+
+  actual <- mod_model_results_distribution_get_data("id", selected_measure_expected)
+
+  expect_equal(actual, "cosmos results")
+  expect_called(m, 1)
+  expect_args(m, 1, "id", "aae_type-01", "ambulance")
+})
+
+test_that("density_plot returns a ggplot", {
+  p <- mod_model_results_distibution_density_plot(
+    model_run_distribution_expected,
+    FALSE
+  )
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("beeswarm returns a ggplot", {
+  p <- mod_model_results_distibution_beeswarm_plot(
+    model_run_distribution_expected,
+    FALSE
+  )
+  expect_s3_class(p, "ggplot")
+})
+
+test_that("plot returns a plotly subplot", {
+  m_density <- mock("density")
+  m_beeswarm <- mock("beeswarm")
+  m_ggplotly <- mock("p_density", "p_beeswarm")
+  m_subplot <- mock("subplot")
+
+  stub(mod_model_results_distibution_plot, "mod_model_results_distibution_density_plot", m_density)
+  stub(mod_model_results_distibution_plot, "mod_model_results_distibution_beeswarm_plot", m_beeswarm)
+  stub(mod_model_results_distibution_plot, "plotly::ggplotly", m_ggplotly)
+  stub(mod_model_results_distibution_plot, "plotly::subplot", m_subplot)
+  stub(mod_model_results_distibution_plot, "plotly::layout", "layout")
+
+  p <- mod_model_results_distibution_plot(model_run_distribution_expected, FALSE)
+  expect_equal(p, "layout")
+
+  expect_called(m_density, 1)
+  expect_args(m_density, 1, model_run_distribution_expected, FALSE)
+
+  expect_called(m_beeswarm, 1)
+  expect_args(m_beeswarm, 1, model_run_distribution_expected, FALSE)
+
+  expect_called(m_ggplotly, 2)
+  expect_args(m_ggplotly, 1, "density")
+  expect_args(m_ggplotly, 2, "beeswarm")
+
+  expect_called(m_subplot, 1)
+  expect_args(m_subplot, 1, "p_density", "p_beeswarm", nrows = 2)
+})
+
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+# server
+# ──────────────────────────────────────────────────────────────────────────────────────────────────────────────────────
+
+test_that("it creates a mod_measure_selection_server", {
+  m <- mock(selected_measure_expected)
+  stub(mod_model_results_distribution_server, "mod_measure_selection_server", m)
+
+  selected_model_run_id <- reactiveVal()
+  data_cache <- cachem::cache_mem()
+
+  shiny::testServer(mod_model_results_distribution_server, args = list(selected_model_run_id, data_cache), {
+    expect_called(m, 1)
+    expect_equal(selected_measure, selected_measure_expected)
+  })
+})
+
+test_that("it calls mod_model_results_distribution_get_data", {
+  m <- mock(model_run_distribution_expected)
+  stub(mod_model_results_distribution_server, "mod_measure_selection_server", reactiveVal)
+  stub(mod_model_results_distribution_server, "mod_model_results_distribution_get_data", m)
+
+  selected_model_run_id <- reactiveVal()
+  data_cache <- cachem::cache_mem()
+
+  shiny::testServer(mod_model_results_distribution_server, args = list(selected_model_run_id, data_cache), {
+    selected_model_run_id("id")
+    selected_measure(selected_measure_expected)
+
+    expect_equal(selected_data(), model_run_distribution_expected)
+
+    expect_called(m, 1)
+    expect_args(m, 1, "id", selected_measure_expected)
+  })
+})
+
+test_that("it renders the plot", {
+  m <- mock()
+
+  stub(
+    mod_model_results_distribution_server, "mod_measure_selection_server", reactiveVal
+  )
+  stub(
+    mod_model_results_distribution_server, "mod_model_results_distribution_get_data", model_run_distribution_expected
+  )
+  stub(
+    mod_model_results_distribution_server, "mod_model_results_distibution_plot", m
+  )
+
+  selected_model_run_id <- reactiveVal("id")
+  data_cache <- cachem::cache_mem()
+
+  shiny::testServer(mod_model_results_distribution_server, args = list(selected_model_run_id, data_cache), {
+    selected_model_run_id("id")
+    selected_measure(selected_measure_expected)
+    session$setInputs(show_origin = FALSE)
+
+    expect_called(m, 1)
+    expect_args(m, 1, model_run_distribution_expected, FALSE)
+  })
+})
