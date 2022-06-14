fba6b13be8e86a644abf59a81bbbb2d25a81f2bb
Tom Jemmett <thomas.jemmett@nhs.net>
2022-06-14 14:28:02 +0100

    improves test by stubbing modules

--- tests/testthat/test-app_ui.R
+++ tests/testthat/test-app_ui.R
@@ -1,3 +1,14 @@
+library(mockery)
+
 test_that("ui is created correctly", {
+  stub(app_ui, "mod_result_selection_ui", "result_selection")
+  stub(app_ui, "mod_params_upload_ui", "params_upload_ui")
+  stub(app_ui, "mod_running_models_ui", "running_models")
+  stub(app_ui, "mod_principal_high_level_ui", "principal_high_level")
+  stub(app_ui, "mod_principal_detailed_ui", "principal_detailed")
+  stub(app_ui, "mod_principal_change_factor_effects_ui", "principal_change_factor_effects")
+  stub(app_ui, "mod_model_core_activity_ui", "model_core_activity")
+  stub(app_ui, "mod_model_results_distribution_ui", "model_results_distribution")
+  stub(app_ui, "mod_capacity_beds_ui", "capacity_beds")
   expect_snapshot(app_ui())
 })
