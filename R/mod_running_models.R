0b0f23f0321d46c6d87b01df15d5b9c2932ff277
Tom Jemmett <thomas.jemmett@nhs.net>
2023-02-07 13:41:17 +0000

    creates explicit reactives for running models tables

--- R/mod_running_models.R
+++ R/mod_running_models.R
@@ -24,13 +24,13 @@ mod_running_models_server <- function(id) {
   shiny::moduleServer(id, function(input, output, session) {
     refresh_timer <- shiny::reactiveTimer(5000)
 
-    output$pool_state <- shiny::renderTable({
+    pool_state_data <- shiny::reactive({
       batch_get_pools() |>
         dplyr::select("id", tidyselect::ends_with("Nodes"))
     }) |>
       shiny::bindEvent(refresh_timer())
 
-    output$running_models <- shiny::renderTable({
+    running_models_data <- shiny::reactive({
       job_status <- function(job) {
         job_id <- job$id
 
@@ -66,5 +66,13 @@ mod_running_models_server <- function(id) {
         purrr::map_dfr(job_status)
     }) |>
       shiny::bindEvent(refresh_timer())
+
+    output$pool_state <- shiny::renderTable({
+      pool_state_data()
+    })
+
+    output$running_models <- shiny::renderTable({
+      running_models_data()
+    })
   })
 }
