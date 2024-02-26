6c21c14a2aa666cfb3f5210bfeeb41e87f71cb29
Tom Jemmett <thomas.jemmett@nhs.net>
2024-02-26 12:21:51 +0000

    patches server_get_results to load from local when running dev/run_dev.R

--- dev/run_dev.R
+++ dev/run_dev.R
@@ -3,9 +3,15 @@ readRenviron(".Renviron")
 
 options(golem.app.prod = FALSE)
 golem::detach_all_attached()
-golem::document_and_reload()
+golem::document_and_reload(export_all = TRUE)
+
+mockery::stub(
+  run_app,
+  "server_get_results",
+  depth = 2,
+  \(...) get_results_from_local("inst/sample_results.json")
+)
 
 # need to explicitly print for VS Code debugger to work
 app <- run_app()
 print(app)
-
