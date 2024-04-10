7131642b00ac3f02c9b56bec389010e4ad747656
Tom Jemmett <thomas.jemmett@nhs.net>
2024-04-10 12:57:23 +0100

    switches to shiny::runApp

--- dev/watch.R
+++ dev/watch.R
@@ -1,15 +1,13 @@
-withr::with_dir(
-  "results_selection_app",
-  {
-    fn <- \() {
-      source("app.R")
-      print(app)
-    }
+selection_app <- callr::r_bg(\() {
+  app <- shiny::runApp(
+    "results_selection_app",
+    port = 9081
+  )
+  print(app)
+})
 
-    results_selection_app <- callr::r_bg(fn)
-  }
-)
 cat("Results Selection App: http://127.0.0.1:9081\n")
+
 
 watchr::watch_files_and_start_task(
   \() {
