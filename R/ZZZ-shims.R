87c6e066a211ea8738bc0e845de8fe73f4de4961
Rhian Davies <thetrianglegirl@gmail.com>
2026-03-02 14:39:30 +0000

    :llama: Lint shim

--- R/ZZZ-shims.R
+++ R/ZZZ-shims.R
@@ -2,9 +2,11 @@ shim_default <- function(results) {
   results |>
     purrr::pluck("results", "default") |>
     dplyr::mutate(
-      model_runs = purrr::map2(baseline, model_runs, \(x, y) c(x, y))
+      model_runs = purrr::map2(.data$baseline, .data$model_runs, c)
     ) |>
-    dplyr::select(c(pod, sitetret, measure, value = model_runs)) |>
-    dplyr::mutate(model_run = list(0:256)) |> # Won't work for different run sizes
-    tidyr::unnest(cols = c(model_run, value))
+    dplyr::select(c("pod", "sitetret", "measure", value = "model_runs")) |>
+    dplyr::mutate(
+      model_run = purrr::map(.data$value, \(x) seq_along(x) - 1)
+    ) |>
+    tidyr::unnest(cols = c("model_run", "value"))
 }
