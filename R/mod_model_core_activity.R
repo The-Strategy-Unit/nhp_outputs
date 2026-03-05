eb4b83dece040526b43324116b810e5f50855a63
Rhian Davies <thetrianglegirl@gmail.com>
2026-03-05 16:20:09 +0000

    ♻️ Use reskit for activity summary

--- R/mod_model_core_activity.R
+++ R/mod_model_core_activity.R
@@ -43,82 +43,29 @@ mod_model_core_activity_ui <- function(id) {
   )
 }
 
-mod_model_core_activity_server_table <- function(
-  data,
-  value_type = c("median", "principal")
-) {
-  value_type <- match.arg(value_type)
-  data_prep <- data |>
-    dplyr::mutate(
-      change = .data[[value_type]] - .data$baseline,
-      change_pcnt = .data$change / .data$baseline,
-      measure = .data$measure |>
-        stringr::str_to_sentence() |>
-        stringr::str_replace("_", "-") |> # tele_attendances
-        stringr::str_replace("Beddays", "Bed days"),
-      activity_type_name = factor(
-        .data$activity_type_name,
-        levels = c("Inpatients", "Outpatients", "A&E")
-      )
-    ) |>
-    dplyr::arrange(.data$activity_type_name, .data$pod_name) |>
-    dplyr::select(
-      "activity_type_name",
-      "pod_name",
-      "measure",
-      "baseline",
-      .env$value_type,
-      "change",
-      "change_pcnt",
-      "lwr_pi",
-      "upr_pi"
-    )
-
-  data_prep |>
-    gt::gt(groupname_col = c("activity_type_name", "pod_name")) |>
-    gt::fmt_integer(c(
-      "baseline",
-      .env$value_type,
-      "change",
-      "lwr_pi",
-      "upr_pi"
-    )) |>
-    gt::fmt_percent("change_pcnt", decimals = 0) |>
-    gt::cols_align(align = "left", columns = "measure") |>
-    gt::cols_label_with(fn = stringr::str_to_title) |>
-    gt::cols_label(
-      "change_pcnt" = gt::html("Percent<br>Change"),
-      "lwr_pi" = "Lower",
-      "upr_pi" = "Upper"
-    ) |>
-    gt::tab_spanner(
-      "80% prediction interval",
-      c("lwr_pi", "upr_pi")
-    ) |>
-    gt_theme()
-}
-
 #' model_core_activity Server Functions
 #'
 #' @noRd
 mod_model_core_activity_server <- function(id, selected_data, selected_site) {
   shiny::moduleServer(id, function(input, output, session) {
-    atpmo <- get_activity_type_pod_measure_options()
-
-    summarised_data <- shiny::reactive({
-      selected_data() |>
-        get_model_core_activity(selected_site()) |>
-        dplyr::inner_join(atpmo, by = c("pod", "measure" = "measures"))
-    })
-
     output$core_activity_median <- gt::render_gt({
-      summarised_data() |>
-        mod_model_core_activity_server_table(value_type = "median")
+      selected_data() |>
+        shim_default() |>
+        reskit::compile_distribution_summary_data(
+          value_type = "median",
+          sites = selected_site()
+        ) |>
+        reskit::make_distribution_summary_table()
     })
 
     output$core_activity_principal <- gt::render_gt({
-      summarised_data() |>
-        mod_model_core_activity_server_table(value_type = "principal")
+      selected_data() |>
+        shim_default() |>
+        reskit::compile_distribution_summary_data(
+          value_type = "principal",
+          sites = selected_site()
+        ) |>
+        reskit::make_distribution_summary_table()
     })
   })
 }
