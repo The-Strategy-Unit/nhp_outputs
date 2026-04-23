4b479a1594de2435f2ff4a9d7854a497268d83f6
Rhian Davies <thetrianglegirl@gmail.com>
2026-04-23 22:48:42 +0100

    :house: Use local mitigator lookup

--- R/ZZZ.R
+++ R/ZZZ.R
@@ -89,16 +89,16 @@ server_get_results <- function(session) {
   )
 }
 
-get_mitigator_lookup <- function(
-  mitigator_lookup = app_sys("app", "data", "mitigators.json")
+get_tpma_lookup <- function(
+  tpma_lookup = app_sys("app", "data", "mitigators.json")
 ) {
-  mitigator_lookup |>
+  tpma_lookup |>
     yyjsonr::read_json_file() |>
     purrr::simplify() |>
-    tibble::enframe("strategy", "mitigator_name") |>
+    tibble::enframe("strategy", "tpma_label") |>
     dplyr::mutate(
-      mitigator_code = stringr::str_extract(
-        .data[["mitigator_name"]],
+      tpma_code = stringr::str_extract(
+        .data[["tpma_label"]],
         "[:upper:]{2}-[:upper:]{2}-[:digit:]{3}"
       )
     )
